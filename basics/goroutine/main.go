4d757eb31e890f4e074f2dd6b22714f80f853918
Anup Kanere <kanereanup@gmail.com>
2025-07-13 12:18:25 +0530

    goroutine
    goroutine example added

---
+++ basics/goroutine/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func increment2(num int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("Go routine Thread!!!")
+	for i := 1; i <= 10; i++ {
+		num += 1
+	}
+	fmt.Println("Final Num with Waitgroup: ", num)
+}
+
+func increment(num int) {
+	fmt.Println("Go routine Thread!!!")
+	for i := 1; i <= 10; i++ {
+		num += 1
+	}
+	fmt.Println("Final Num : ", num)
+}
+
+func main() {
+	x := 10
+
+	// First method
+	go increment(x)
+	time.Sleep(1 * time.Second)
+	fmt.Println("Main Thread")
+
+	// Second method using sync.WaitGroup
+	var wg sync.WaitGroup
+	wg.Add(1)
+	increment2(x, &wg)
+	wg.Wait()
+	fmt.Println("Main thread Complete!!!")
+
+}
